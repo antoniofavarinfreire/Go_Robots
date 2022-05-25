63e7bf5a879e90afb12165b08a3851023dc5a525
Felipe B <fhullerbr@gmail.com>
2022-05-24 21:26:29 -0300

    fontawesome e footer prontos

--- src/pages/header.php
+++ src/pages/header.php
@@ -6,9 +6,17 @@
     
     <!-- Folhas de estilo -->
     <link rel="stylesheet" href="src/assets/css/style.css">
+    <link rel="stylesheet" href="src/assets/css/all.min.css">
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    
     
 </head>
+
+<header>
+    <div class="header">
+        <a href="http://"></a>
+    </div>
+</header>
+
 <body>
